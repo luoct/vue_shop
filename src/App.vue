148c83c93dd9c2b5a4e9418805dfa5a3e7c57916
luoct <1422291209@qq.com>
2021-10-18 11:15:09 +0800

    在App中添加了全局样式，不知道有没有影响

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,14 @@ export default {
   #app {
     height: 100%;
   }
+  .card {
+    box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
+    border-radius: 4px;
+    border: 1px solid #ebeef5;
+    margin-top: 20px;
+    padding: 20px;
+  }
+  .search {
+    padding-bottom: 20px;
+  }
 </style>
---
+++ src/components/goods/Params.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品参数</el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="card">
+      <div class="search">
+        <el-button
+          type="primary"
+          @click="showAddCateDialog"
+        >添加商品分类</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
