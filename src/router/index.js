3ef6ab6784454a35b7abf528e5d9fa136984481d
luoct <1422291209@qq.com>
2021-10-20 09:34:57 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '@/components/Login'
-import Home from '@/components/Home'
-import Welcome from '@/components/Welcome'
-import Users from '@/components/user/Users'
-import Rights from '@/components/power/Rights'
-import Roles from '@/components/power/Roles'
-import Cate from '@/components/goods/Cate'
-import Params from '@/components/goods/Params'
-import List from '@/components/goods/List'
-import Add from '@/components/goods/Add'
-import Order from '@/components/order/Order'
-import Report from '@/components/report/Report'
+
+// import Login from '@/components/Login'
+// import Home from '@/components/Home'
+// import Welcome from '@/components/Welcome'
+const Login = () => import(/* webpackChunkName: "group-index" */ '@/components/Login')
+const Home = () => import(/* webpackChunkName: "group-index" */ '@/components/Home')
+const Welcome = () => import(/* webpackChunkName: "group-index" */ '@/components/Welcome')
+
+// import Users from '@/components/user/Users'
+// import Rights from '@/components/power/Rights'
+// import Roles from '@/components/power/Roles'
+const Users = () => import(/* webpackChunkName: "group-user" */ '@/components/user/Users')
+const Rights = () => import(/* webpackChunkName: "group-user" */ '@/components/power/Rights')
+const Roles = () => import(/* webpackChunkName: "group-user" */ '@/components/power/Roles')
+
+// import Cate from '@/components/goods/Cate'
+// import Params from '@/components/goods/Params'
+// import List from '@/components/goods/List'
+// import Add from '@/components/goods/Add'
+const Cate = () => import(/* webpackChunkName: "group-goods" */ '@/components/goods/Cate')
+const Params = () => import(/* webpackChunkName: "group-goods" */ '@/components/goods/Params')
+const List = () => import(/* webpackChunkName: "group-goods" */ '@/components/goods/List')
+const Add = () => import(/* webpackChunkName: "group-goods" */ '@/components/goods/Add')
+
+// import Order from '@/components/order/Order'
+// import Report from '@/components/report/Report'
+const Order = () => import(/* webpackChunkName: "group-o" */ '@/components/order/Order')
+const Report = () => import(/* webpackChunkName: "group-o" */ '@/components/report/Report')
 
 
 
